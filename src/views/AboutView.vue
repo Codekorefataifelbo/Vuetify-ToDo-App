5ef5b035ebaadd68d85bc3f10fe91d29fe6ab32e
Arif Ul Hoque <arif.hoque123@gmail.com>
2024-04-15 17:11:56 +0600

    Implmented ToDo Page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-10">
+    <h1>About Vuetify TO-DO APP</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta nulla doloremque ratione vero veritatis, error, nemo, reprehenderit accusamus aperiam commodi fugit laboriosam impedit repellendus ipsa quaerat sequi. Repellendus, voluptate aspernatur?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta nulla doloremque ratione vero veritatis, error, nemo, reprehenderit accusamus aperiam commodi fugit laboriosam impedit repellendus ipsa quaerat sequi. Repellendus, voluptate aspernatur?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta nulla doloremque ratione vero veritatis, error, nemo, reprehenderit accusamus aperiam commodi fugit laboriosam impedit repellendus ipsa quaerat sequi. Repellendus, voluptate aspernatur?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta nulla doloremque ratione vero veritatis, error, nemo, reprehenderit accusamus aperiam commodi fugit laboriosam impedit repellendus ipsa quaerat sequi. Repellendus, voluptate aspernatur?</p>
   </div>
 </template>
